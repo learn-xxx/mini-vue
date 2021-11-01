fb7524daa66ebf91ce65f2049e881fbadc0c49ec
Merlin218 <61051874+Merlin218@users.noreply.github.com>
2021-11-01 10:17:09 +0800

    实现 effect 返回 runner

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -8,8 +8,9 @@ class ReactiveEffect {
   run() {
     try {
       activeEffect = this;
-      this._fn();
+      return this._fn();
     } finally {
+      //todo
     }
   }
 }
@@ -20,14 +21,26 @@ export function effect(fn) {
   const _effect = new ReactiveEffect(fn);
   //最开始调用一次
   _effect.run();
+
+  return _effect.run.bind(_effect);
+
+  //bind:创建一个新函数，使函数的this指向传入的第一个参数，其他参数作为新函数的参数
 }
 
+const targetMap = new WeakMap();
 // targetMap:{
 //   [target:Map]:{
 //     [key: Set]:[]
 //   }
 // }
-const targetMap = new WeakMap();
+
+//WeakMap和Map的区别
+// 1、WeakMap只接受对象作为key，如果设置其他类型的数据作为key，会报错。
+// 2、WeakMap的key所引用的对象都是弱引用，只要对象的其他引用被删除，垃圾回收机制就会释放该对象占用的内存，从而避免内存泄漏。
+// 3、由于WeakMap的成员随时可能被垃圾回收机制回收，成员的数量不稳定，所以没有size属性。
+// 4、没有clear()方法
+// 5、不能遍历
+
 //把依赖添加到targetMap对应target的key中，在重新set时在trigger中重新触发
 export function track(target: Object, key) {
   if (!activeEffect) {
